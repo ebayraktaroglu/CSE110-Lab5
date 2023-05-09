fbc930aa611655238a695e58a12b384c021da4cc
ebayraktaroglu <97638717+ebayraktaroglu@users.noreply.github.com>
2023-05-08 18:50:32 -0700

    I did explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,52 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  	const synth = window.speechSynthesis;
+
+  	const button = document.querySelector("button");
+	const voiceSelect = document.querySelector("select");
+	const text = document.querySelector("textarea");
+	const faceImage = document.querySelector("img");
+	let voices = [];
+
+	function populateVoiceList() {
+  		voices = synth.getVoices();
+  		for (let i = 0; i < voices.length; i++) {
+    		const option = document.createElement("option");
+    		option.textContent = `${voices[i].name} (${voices[i].lang})`;
+		
+    		if (voices[i].default) {
+     			option.textContent += " — DEFAULT";
+    		}
+
+    		option.setAttribute("data-lang", voices[i].lang);
+    		option.setAttribute("data-name", voices[i].name);
+    		voiceSelect.appendChild(option);
+  		}
+	}	
+
+	populateVoiceList();
+	if (speechSynthesis.onvoiceschanged !== undefined) {
+  		speechSynthesis.onvoiceschanged = populateVoiceList;
+	}
+
+
+
+	button.onclick = (event) => {
+  		event.preventDefault();
+  		const utterThis = new SpeechSynthesisUtterance(text.value);
+		utterThis.onstart = (event) => {
+			faceImage.src = 'assets/images/smiling-open.png';
+		}
+		utterThis.onend = (event) => {
+			faceImage.src = 'assets/images/smiling.png';
+		}
+  		const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+  		for (let i = 0; i < voices.length; i++) {
+    		if (voices[i].name === selectedOption) {
+      			utterThis.voice = voices[i];
+    		}
+  		}
+  		synth.speak(utterThis);
+	};
 }
